58f754f754178f1cca76aaf40a482c5ce7dc4d06
Евгений <egorzhek@gmail.com>
2021-08-28 10:43:56 +0300

    Запрос по структуре Пифа

---
+++ SqlScripts/6_FundStructure/Запрос по структуре Пифов2.sql
@@ -0,0 +1,40 @@
+Declare
+    @Date Date = CONVERT(Date, '31.01.2009', 103),
+    @Contract_Id Int = 17593;
+
+
+select
+	Investment, VALUE_ID, VALUE_RUR = sum(VALUE_RUR), Result = sum(VALUE_RUR)/AllSum
+from
+(
+    select
+        Inv.Investment,
+        res.VALUE_ID,
+        res.VALUE_RUR,
+        AllSum = sum(res.VALUE_RUR) over()
+    from
+    (
+        select
+            Contract_Id, PortfolioDate, Investor_Id, Investment_id,
+            VALUE_ID, BAL_ACC, CLASS, AMOUNT,
+            BAL_SUMMA_RUR, Bal_Delta, NOMINAL, RUR_PRICE,
+            Nom_Price, VALUE_RUR, VALUE_NOM, CUR_ID,
+            RATE, RATE_DATE, RecordDate
+        from [dbo].[FundStructure] nolock
+        where Contract_Id = @Contract_Id and PortfolioDate = @Date
+        union all
+        select
+            Contract_Id, PortfolioDate, Investor_Id, Investment_id,
+            VALUE_ID, BAL_ACC, CLASS, AMOUNT,
+            BAL_SUMMA_RUR, Bal_Delta, NOMINAL, RUR_PRICE,
+            Nom_Price, VALUE_RUR, VALUE_NOM, CUR_ID,
+            RATE, RATE_DATE, RecordDate
+        from [dbo].[FundStructure_Last] nolock
+        where Contract_Id = @Contract_Id and PortfolioDate = @Date
+    ) as res
+    join [dbo].[InvestmentIds] as Inv on res.Investment_id = Inv.Id
+    left join [dbo].[ClassCategories] as cs on res.CLASS = cs.ClassId
+    left join [dbo].[Categories] as c on cs.CategoryId = c.Id
+) as res2
+where VALUE_RUR > 0
+group by Investment, VALUE_ID, AllSum
