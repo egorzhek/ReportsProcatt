212aafac3f3cbae53a76afe336c5edff940fa61f
Dmitry Golotin <d.golotin@gmail.com>
2021-09-04 13:42:11 +0300

    Create Model Pattern

--- ReportsProcatt/Models/Report.cs
+++ ReportsProcatt/Models/Report.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,40 @@ namespace ReportsProcatt.Models
 {
     public class Report
     {
+        public DateTime Dfrom { get; set; }
+        public DateTime Dto { get; set; }
+        public string ReportCurrency { get; set; }
+        public string ReportCurrencyChar { get; set; }
         public string rootStr { get; set; }
+        public Headers MainHeader { get; set; }
+        public List<DiagramElement> DiagramElements { get; set; }
+        public DataTable PIFsTotals { get; set; }
+        public DataTable DUsTotals { get; set; }
+        public DataTable AllAssets { get; set; }
+        public DataTable DivsNCoupons { get; set; }
+        public DataTable DivsNCouponsDetails { get; set; }
+        public CircleDiagram Assets { get; set; }
+        public CircleDiagram Instruments { get; set; }
+        public CircleDiagram Currency { get; set; }
+        public List<PIF> PIFs { get; set; }
+        public List<DU> DUs { get; set; }
+        public class Headers
+        {
+            public string TotalSum { get; set; }
+            public string ProfitSum { get; set; }
+            public string Return { get; set; }
+        }
+        public class DiagramElement
+        {
+            public string Code { get; set; }
+            public string Name { get; set; }
+            public string Value { get; set; }
+        }
+        public class CircleDiagram { }
+        public class PIF
+        {
+
+        }
+        public class DU { }
     }
 }
